ed2a05e10bbb2e3e5f486698620ccf115be80996
Ken LaRose <kmlarose2@gmail.com>
2017-07-06 15:18:28 -0500

    class for database model and basic menu loop

--- work_log.py
+++ work_log.py
@@ -1,5 +1,39 @@
+import datetime
+
+from peewee import *
+
+db = SqliteDatabase('work_log.db')
+
+
+class Entry(Model):
+    """Database model for Work Log Entries"""
+    worker_name = TextField()
+    task_name = TextField()
+    task_time = IntegerField()
+    task_notes = TextField()
+    created = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = db
+
+
+def initialize():
+    """Create the database and the table if they don't exist"""
+    db.connect()
+    db.create_tables([Entry], safe=True)
+
+
+def menu_loop():
+    """Show the menu"""
+    menu_choice = None
+
+    while menu_choice != 'q':
+        menu_choice = input('Enter q to quit: ').lower()
+
 # As a user of the script, I should be able to choose whether to add a new entry or lookup previous entries.
-# As a user of the script, if I choose to enter a new work log, I should be able to provide my name, a task name, a number of minutes spent working on it, and any additional notes I want to record.
+# As a user of the script, if I choose to enter a new work log, I should be able to provide my name, a task name,
+# a number of minutes spent working on it, and any additional notes I want to record.
+
 # As a user of the script, if I choose to find a previous entry, I should be presented with four options: find by employee, find by date, find by time spent, find by search term.
 # As a user of the script, if finding by employee, I should be presented with a list of employees with entries and be able to choose one to see entries from.
 # As a user of the script, if finding by date, I should be presented with a list of dates with entries and be able to choose one to see entries from.
@@ -22,4 +56,5 @@
 # TODO-kml: create the database
 
 if __name__ == '__main__':
-    pass
+    initialize()
+    menu_loop()
